79059670c25476d4b35071a0a23c50527ee1eb45
Gareth Fultz <garethfultz@gmail.com>
2025-03-19 11:58:14 -0400

    Removed unused wrapper types

--- src/codegen/util/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod builder;
-pub mod context;
-pub mod types;
-pub mod values;
-pub mod module;
--- src/codegen/util/types/float.rs
+++
@@ -1,9 +0,0 @@
-use crate::{codegen::util::values::*, wrapper_type};
-
-wrapper_type!(FloatType: FloatType);
-
-impl<'ctx> FloatType<'ctx> {
-    pub fn const_float(&mut self, value: f64) -> FloatValue<'ctx> {
-        FloatValue::new(self.value.const_float(value))
-    }
-}
